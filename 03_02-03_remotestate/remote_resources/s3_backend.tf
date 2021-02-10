3cee78627510bf4bd739ae997693a40ab4c64690
blood-onix <blood-onix@ya.ru>
2021-02-10 13:00:17 +0300

    region correction

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -16,7 +16,7 @@ variable "bucket_name" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = eu-central-1
+  region = "eu-central-1"
 }
 
 # //////////////////////////////
