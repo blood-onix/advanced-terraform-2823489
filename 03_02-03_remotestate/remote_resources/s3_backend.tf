382312f0bcb038a08417e7e2bc912f0ad51a6634
blood-onix <blood-onix@ya.ru>
2021-02-10 12:56:25 +0300

    upd names

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -29,7 +29,7 @@ data "aws_iam_user" "terraform" {
 # //////////////////////////////
 # S3 BUCKET
 # //////////////////////////////
-resource "aws_s3_bucket" "red30-tfremotestate" {
+resource "aws_s3_bucket" "bloodterra-tfstate" {
   bucket = var.bucket_name
   force_destroy = true
   acl = "private"
@@ -57,8 +57,8 @@ resource "aws_s3_bucket" "red30-tfremotestate" {
 EOF
 }
 
-resource "aws_s3_bucket_public_access_block" "red30-tfremotestate" {
-  bucket = aws_s3_bucket.red30-tfremotestate.id
+resource "aws_s3_bucket_public_access_block" "bloodterra-tfstate" {
+  bucket = aws_s3_bucket.bloodterra-tfstate.id
 
   block_public_acls   = true
   block_public_policy = true
@@ -70,7 +70,7 @@ resource "aws_s3_bucket_public_access_block" "red30-tfremotestate" {
 # DYNAMODB TABLE
 # //////////////////////////////
 resource "aws_dynamodb_table" "tf_db_statelock" {
-  name           = "red30-tfstatelock"
+  name           = "bloodterra-tfstatelock"
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "LockID"
